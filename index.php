8902ff049360c83e96c030fd2897a82912bf9886
Maxime <maxime.loir@lepoles.org>
2024-05-27 16:51:40 +0200

    27/05/24

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'public/header.html.php';
+?>
+<?php
+include 'public/nav.html.php';
+?>
+<main></main>
+<?php
+include 'public/footer.html.php';
+?> 
---
+++ public/header.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon premier site en HTML/CSS</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+<body>
+    <div id="container">
+    <header>
+        <div><img src="public/assets/img/logo.png" alt=""></div>
+        
+
+    <h1>Mon premier site en HTML/CSS </h1>
+    <div><i class="fa-brands fa-instagram fa-2xl" style="color: #d10099;"></i><br><br><i class="fa-brands fa-square-facebook fa-2xl" style="color: #0043b8;"></i></div>
+
+
+</header>
+    
+
