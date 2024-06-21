d801f47c6101110d1aca528a272b3064ef7c234a
Maxime <maxime.loir@lepoles.org>
2024-06-21 16:58:30 +0200

    21/06/24

--- image.php
+++ image.php
@@ -5,3 +5,13 @@ include 'public/header.html.php';
 <?php
 include 'public/footer.html.php';
 ?> 
+
+    <div class="container">
+        <h2>Images</h2>
+        <h3>Image de fond en CSS</h3>
+        <div class="image-container">
+            <div class="overlay">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quas recusandae iure, laudantium incidunt totam veniam. Error sequi et pariatur, necessitatibus placeat soluta, libero eveniet accusamus, ipsa velit corporis provident labore! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquid quas voluptas nulla, laboriosam voluptate reiciendis commodi vel! Magni nisi, fuga eum asperiores officia commodi laboriosam ea quae justo esse dolores.</p>
+            </div>
+        </div>
+    </div>
--- liste.php
+++ liste.php
@@ -1,7 +1,34 @@
 <?php
 include 'public/header.html.php';
 ?>
-<main class="liste">liste</main>
+<main class="liste"></main>
 <?php
 include 'public/footer.html.php';
 ?> 
+
+        <h2>Listes</h2>
+        <h3>Les listes non-ordonnées - ul</h3>
+        <ul>
+            <li><img src="https://img.icons8.com/material-outlined/24/000000/orange.png" alt="Orange"/> Orange</li>
+            <li><img src="https://img.icons8.com/material-outlined/24/000000/strawberry.png" alt="Fraise"/> Fraise</li>
+            <li><img src="https://img.icons8.com/material-outlined/24/000000/pear.png" alt="Poire"/> Poire</li>
+        </ul>
+        <h3>Les listes ordonnées - ol</h3>
+        <ol>
+            <li>Html/Css</li>
+            <li>PHP/SQL</li>
+            <li>JS/JQUERY</li>
+        </ol>
+        <h3>Listes imbriquées</h3>
+        <ul>
+            <li>Europe</li>
+            <li>Asie</li>
+        </ul>
+        <h3>Listes de définitions</h3>
+        <dl>
+            <dt>F</dt>
+            <dd>Foxtrot</dd>
+            <dd>France</dd>
+            <dd>Food</dd>
+        </dl>
+    </div>
